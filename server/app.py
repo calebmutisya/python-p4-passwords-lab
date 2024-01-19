65877749720ca5323ae48d5a4d331a359affd8eb
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-19 11:54:18 +0300

    Start

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session,jsonify,make_response
 from flask_restful import Resource
 
 from config import app, db, api
